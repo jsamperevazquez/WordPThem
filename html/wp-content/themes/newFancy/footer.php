acc48e574778f2130fe9cbad5435ca1fca9683af
jsamperevazquez <jsamperevazquez@danielcastelao.org>
2021-11-23 09:36:11 +0100

    Actualizado header y footer

--- html/wp-content/themes/newFancy/footer.php
+++ html/wp-content/themes/newFancy/footer.php
@@ -1,3 +1,6 @@
-<h1 style="text-align: center; font-family: Courier,math; font-size: medium">Pie de paǵina</h1>
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri();?>">
+<div id="pie">
+    <h1><br><br>Pie de paǵina</h1>
+</div>
 
-<?php
+
--- html/wp-content/themes/newFancy/header.php
+++ html/wp-content/themes/newFancy/header.php
@@ -1,4 +1,4 @@
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri();?>">
 <div id="cabecera"></div>
 <h1 style="text-align: center; color: #00e676;  text-transform: uppercase">Mi tema Funcy</h1>
-
 <?php
